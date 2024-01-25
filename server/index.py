02c840bd9c100fabbeeb238d4120f2f37cfeccc4
lemonorangeapple <107529938+lemonorangeapple@users.noreply.github.com>
2024-01-25 18:27:54 +0800

    Update index.py

--- server/index.py
+++ server/index.py
@@ -1,13 +1,18 @@
 import dns.resolver
 from flask import *
+dns.resolver.name = ['1.0.0.1', '8.8.4.4']
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     add = request.values.get('get')
+    add = add.split(';')
     if add is None:
         return '200 OK'
     try:
-        return str(add + ' ' + dns.resolver.resolve(add, 'A')[0].to_text())
+        res = ''
+        for i in add:
+            res += str(i + ' ' + dns.resolver.resolve(i, 'A')[0].to_text()) + '\n'
+        return res
     except Exception as e:
         return str(e), 500
